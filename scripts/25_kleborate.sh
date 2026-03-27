38d23389f7abd2b764f161b0ab6b12e4f26a66b9
Maida Jajja <maidaj14@gmail.com>
2026-03-27 00:14:32 +0000

    Add Kleborate script

---
+++ scripts/25_kleborate.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=kleborate_kp
+#SBATCH --partition=msc_appbio
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=20G
+#SBATCH --time=06:00:00
+#SBATCH --output=/scratch/users/k22017808/KP_Research_Project/09_Kleborate/kleborate_%j.out
+#SBATCH --error=/scratch/users/k22017808/KP_Research_Project/09_Kleborate/kleborate_%j.err
+
+source /software/spackages_v0_21_prod/apps/linux-ubuntu22.04-zen2/gcc-13.2.0/anaconda3-2022.10-5wy43yh5crcsmws4afls5thwoskzarhe/etc/profile.d/conda.sh
+conda activate kleborate_env
+
+FNADIR=/scratch/users/k22017808/KP_Research_Project/ALL_FNA_FILES
+OUTDIR=/scratch/users/k22017808/KP_Research_Project/09_Kleborate
+
+mkdir -p "$OUTDIR"
+
+echo "Starting Kleborate at $(date)"
+
+kleborate \
+    --assemblies "$FNADIR"/*.fna \
+    --outfile "$OUTDIR/kleborate_results.txt" \
+    --preset kpsc
+
+echo "Finished Kleborate at $(date)"
